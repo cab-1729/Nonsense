d1169b237acc019d74b7f301a65b4b4fe1daf387
cab-1729 <60106638+cab-1729@users.noreply.github.com>
2020-08-28 07:58:40 +0530

    Update nonsense1.py

--- nonsense1.py
+++ nonsense1.py
@@ -37,5 +37,7 @@ for _ in range(args.length-1):
 	word1=word2
 if args.store=='clipboard':
 	copy(nonsense)
+elif args.store=='console':
+	print(nonsense)
 else:
 	with open(args.store,'w') as final:final.write(nonsense)
